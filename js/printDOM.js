4f60d0ddedbe87bf5517de2db33a3ff19c59b21c
Chuck Blackard <37086923+chuckblackard@users.noreply.github.com>
2018-03-12 23:46:54 -0500

    Update printDOM.js

--- js/printDOM.js
+++ js/printDOM.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
   var indexItems = document.getElementsByTagName("*");
-  for(var i = 0, i < indexItems.length; i++)
+  for(var i = 0; i < indexItems.length; i++)
   {
     console.log(index.indexItems[i].tagName);
   }
