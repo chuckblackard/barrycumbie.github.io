6097902ec9185d9dfa0f144ce5ee4470e2fbc082
Chuck Blackard <37086923+chuckblackard@users.noreply.github.com>
2018-03-12 23:34:26 -0500

    Create printDOM.js

---
+++ js/printDOM.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+  var indexItems = document.getElementsByTagName("*");
+  for(var i = 0, i < indexItems.length; i++)
+  {
+    console.log(index.indexItems[i].tagName);
+  }
+});
